7e3a49ff19f3a00626d31633a9067c15e5bc0b19
Lucio Pintanel <lm.pintanel@gmail.com>
2021-09-07 15:00:44 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsvenda/DsvendaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsvenda/DsvendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
